32074cbd61847acf95e402c175f81c081edc4490
Montial Charline <charline.montial@epfl.ch>
2019-09-26 09:37:32 +0200

    Correct test

--- byzcoin/webpage_test.go
+++ byzcoin/webpage_test.go
@@ -3,12 +3,15 @@ package byzcoin
 import (
 	"testing"
 	"time"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/stretchr/testify/require"
 	"go.dedis.ch/cothority/v3"
 	"go.dedis.ch/cothority/v3/byzcoin"
 	"go.dedis.ch/cothority/v3/darc"
 	"go.dedis.ch/onet/v3"
+	"go.dedis.ch/protobuf"
 )
 
 func TestValue_Spawn(t *testing.T) {
@@ -28,18 +31,17 @@ func TestValue_Spawn(t *testing.T) {
 	cl, _, err := byzcoin.NewLedger(genesisMsg, false)
 	require.Nil(t, err)
 
-	myURL := []byte("http://www.mlppreservationproject.com/")
+	URLArg := []byte("http://www.mlppreservationproject.com/")
+	resp, _ := http.Get(string(URLArg))
+	content, _ := ioutil.ReadAll(resp.Body)
+
 	ctx, err := cl.CreateTransaction(byzcoin.Instruction{
 		InstanceID: byzcoin.NewInstanceID(gDarc.GetBaseID()),
 		Spawn: &byzcoin.Spawn{
 			ContractID: ContractWebPageID,
 			Args: []byzcoin.Argument{{
 				Name:  "URLWebPage",
-				Value: myURL,
-			},
-			{
-				Name:  "content",
-				Value: myURL,
+				Value: URLArg,
 			}},
 		},
 		SignerCounter: []uint64{1},
@@ -49,12 +51,39 @@ func TestValue_Spawn(t *testing.T) {
 
 	_, err = cl.AddTransaction(ctx)
 	require.Nil(t, err)
-	pr, err := cl.WaitProof(byzcoin.NewInstanceID(ctx.Instructions[0].DeriveID("").Slice()), 2*genesisMsg.BlockInterval, myURL)
-	require.Nil(t, err)
-	require.True(t, pr.InclusionProof.Match(ctx.Instructions[0].DeriveID("").Slice()))
-	v0, _, _, err := pr.Get(ctx.Instructions[0].DeriveID("").Slice())
-	require.Nil(t, err)
-	require.Equal(t, myURL, v0)
 
 	local.WaitDone(genesisMsg.BlockInterval)
+
+	// Polls the blockchain to get a proof for our new instance
+	pr, err := cl.WaitProof(byzcoin.NewInstanceID(ctx.Instructions[0].DeriveID("").Slice()), 2*genesisMsg.BlockInterval, nil)
+	require.Nil(t, err)
+
+	// Checks the proof to ensure the instance is created
+	require.True(t, pr.InclusionProof.Match(ctx.Instructions[0].DeriveID("").Slice()))
+
+	// Gets the encoded content of the contract in the form of a byte slice
+	resultBuf, _, _, err := pr.Get(ctx.Instructions[0].DeriveID("").Slice())
+	require.Nil(t, err)
+
+	// Initializes en empty struct so the resultBuf can be decoded into it
+	result := ContractWebPageData{}
+
+	// Deserializes the byte slice we received into our empty struct
+	err = protobuf.Decode(resultBuf, &result)
+	require.Nil(t, err)
+
+	// Now let's check the content of our struct
+	// result.Storage should contain two KeyValue elements
+	require.Equal(t, 2, len(result.Storage))
+
+	// Checks the first key value
+	require.Equal(t, "URLWebPage", result.Storage[0].Key)
+	require.Equal(t, URLArg, result.Storage[0].Value)
+
+	// Checks the second key value
+	require.Equal(t, "content", result.Storage[1].Key)
+	require.Equal(t, content, result.Storage[1].Value)
+
+	local.WaitDone(genesisMsg.BlockInterval)
+
 }
