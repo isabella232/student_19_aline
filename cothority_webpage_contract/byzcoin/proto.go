f39bb7001ba4032011e996316506391954353474
Montial Charline <charline.montial@epfl.ch>
2019-10-22 18:51:54 +0200

    Remove useless struct and indent

--- cothority_webpage_contract/byzcoin/proto.go
+++ cothority_webpage_contract/byzcoin/proto.go
@@ -1,15 +1,9 @@
 package byzcoin
 
 type ContractWebPageData struct {
-	URLWebPage string
-	Content [32]byte
-	Selector string
+	URLWebPage   string
+	Content      [32]byte
+	Selector     string
 	CreationDate string
-	TextOnly int
-}
-
-type ContractWebPageArgs struct {
-	URLWebPage string
-	Selector string
-	TextOnly int
+	TextOnly     int
 }
