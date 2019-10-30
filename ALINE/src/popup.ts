a08abbb2b45d60397d1e8ec2f4d54c330cd0ab8f
Montial Charline <charline.montial@epfl.ch>
2019-10-30 23:05:06 +0100

    Change roster

--- ALINE/src/popup.ts
+++ ALINE/src/popup.ts
@@ -440,3 +440,40 @@ function hexStringToByte(str: string) {
   
   return new Uint8Array(a);
 }
+
+const roster = `[[servers]]
+Address = "tls://localhost:7774"
+Suite = "Ed25519"
+Public = "b3f31a9cebb85315c2418adb8693cb2671024a6916b5bffbdcb090c85dc1cb1e"
+Description = "Conode_3"
+[servers.Services]
+  [servers.Services.ByzCoin]
+    Public = "22cac009154cf277596fd33fb05ac19c39fd8f59a221594507270f2c3a2024b0351cce13ca847326acd1a3fecce226d8ad8c092889e56a7d8601976e5cc399c71014ac12fcfbde167f4f7686887e713ce2a39551d7593f2e2c1dd5623f9ada5d2f5892ff8b89551bb35ef15afbc5542ee021ae769c510ac6220aecfd4132c9f5"
+    Suite = "bn256.adapter"
+  [servers.Services.Skipchain]
+    Public = "6a9390bbfad62bbded928c8368a30cd418da44104456203dcc225fd4b6b99fba14d4c0c7e2f376cb212b59d080fea3de0fdd44b39b4bdc00b1ed804aef1fff6730fce3ec9c65af9fbf8dc8b1e7af70c67f036f70cf6bb48d5a734dbeddd4219a5f46d42b85e825c29bb6db94b377c99b089484edcaa0531fb73dfc30557ab2cb"
+    Suite = "bn256.adapter"
+[[servers]]
+Address = "tls://localhost:7772"
+Suite = "Ed25519"
+Public = "e982b7583ad7f459c095be59ecca9157769984de83b2d60725a88b3e833178d0"
+Description = "Conode_2"
+[servers.Services]
+  [servers.Services.ByzCoin]
+    Public = "3a46c48d8624a35beb28d392556dd81c2a6251cfa6fc04be264153953ee9cb2d34ae213d271609df90d56353efff32a2286c7b9cf6db44f59dff44f5b1e08479628fa0be26d82ef7f7f611d1de5c9c79043c3527abf0a6126f0336eac18cd7dc2e7e70de93e052eca039b9cad05cf4d3a32241dc976fa158173827aa70072d97"
+    Suite = "bn256.adapter"
+  [servers.Services.Skipchain]
+    Public = "898baf474990bd4f44b4e968f45af279f254efc4cf05162b8a9aef66a4dac81835174f7e25c9a40887014cc0bc1b62edd21bccd1b684f6546ff6b03adc68890639f092d8134330a6c55804accc726f9bed4d3a3c47f7915f54e18dabf94e2d17656ec97c3971f573856c5751a793b749adcc5d4671a5c411e677f7a89a5fe14f"
+    Suite = "bn256.adapter"
+[[servers]]
+Address = "tls://localhost:7770"
+Suite = "Ed25519"
+Public = "f533cc5777794a82af55c11c7f7a12315df11906de4470484b68ea9227d49b0a"
+Description = "Conode_1"
+[servers.Services]
+  [servers.Services.ByzCoin]
+    Public = "280598e71118f8f4e7ed9a2821cc1d557a229466d87a7dd11c4d2b708bb3f6ad338cff17dcc4576b471cbd1cb0028a2c1fecfb72717af81ad25997a1dbca632849b755f0daad1869d3eecfcfa289eb9754d52969b84745d3defe5ae9b9ff6a8435fc69daa3dd74cc10690fd13fed17756734c0a35083f4491266b36b73f46a6b"
+    Suite = "bn256.adapter"
+  [servers.Services.Skipchain]
+    Public = "5473f95f049e706d90452732a71ea478a7075442d97237e7956484474c2633f780dd53f30a9d83d703cb5eb17dfa3b82bf08a9f9a2aff741bab76e0a0809cb891d96a81672214136f83350ce83a70c3ad9fe07dbe2c9bbde8fce44d3a7e242e8462169fa1351a3b37ee2a89693f4d375ac2c0d05d927f13bfafdd5b26a4fdcbc"
+    Suite = "bn256.adapter"`
