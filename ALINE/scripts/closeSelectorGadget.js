ac341e07e396d616f12a9468e098809710f9699c
Unknown <charline.montial@epfl.ch>
2019-11-23 23:55:25 +0100

    Add CSS Selector feature scripts

---
+++ ALINE/scripts/closeSelectorGadget.js
@@ -0,0 +1,2 @@
+// Retrieve CSS Selector in input form
+document.querySelector("#_sg_div > input:nth-child(6)").click()
---
+++ ALINE/scripts/findCSSSelector.js
@@ -0,0 +1,6 @@
+// Retrieve CSS Selector in input form
+    if(document.getElementById('_sg_path_field')!== null){
+        alert("FINALLY !")
+    var you = document.getElementById('_sg_path_field').value;
+    alert(you)
+    }
