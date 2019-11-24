b085c8b75f8d928cb0a74295f4565cd54db7fc69
Unknown <charline.montial@epfl.ch>
2019-11-24 15:50:07 +0100

    Clean code and restaure all features

--- ALINE/src/popup.ts
+++ ALINE/src/popup.ts
@@ -10,60 +10,14 @@ export {
 };
 
 window.onload = function () {
-  
+
   /*---------------------------------------------------------------------
    |  Certify whole page
    *-------------------------------------------------------------------*/
   var checkPageButton = document.getElementById('fullpage');
   if (checkPageButton) {
     checkPageButton.addEventListener('click', function () {
-      alert("You are working !");
-      //spawnWebPageContractWithParameters("html");
-      // Retrieve current URL  
-      chrome.tabs.query({
-        'active': true,
-        'lastFocusedWindow': true
-      }, async function (tabs) {
-
-        var skipChainID = "0191ff647ab0d73bc2b5798077f1b43b6c032435d412a29e8fc24c958d995bf8";
-        var privateKey = "d7ef534d6bc30fd84903e11d15a1d091b17c9f307a20fef1e83bb6fadd1e6404";
-
-        // Retrieve URL of current webpage
-        var url = tabs[0].url;
-
-        // Retrieve TextOnly field
-        const textOnlyBox = document.getElementById('txtOnly') as HTMLInputElement;
-        var textOnly = textOnlyBox.checked
-
-        // ** SPAWN CONTRACT ** //
-        const p = document.getElementById('status');
-
-        // Get public.toml
-        await Handler.getInstance().LoadRoster(roster);
-
-        // Skip chain ID
-        await getDarc(skipChainID);
-
-        // Private Key
-        await loadSigner(privateKey);
-
-        // Add rule to spawn the webPage contract
-        await addRule("spawn:webPage");
-
-        // Spawn the webPage contract
-        var contractWebPageData = new ContractWebPageData();
-        contractWebPageData.URLWebPage = url;
-        contractWebPageData.Selector = "html";
-        contractWebPageData.TextOnly = textOnly;
-
-        //TODO: Try to lower timeout
-        await new Promise(resolve => setTimeout(resolve, 3000))
-        await spawnWebPage(contractWebPageData).then(
-          (r) => Handler.prependLog("instance spawned: " + r)
-        ).catch(
-          (e) => Handler.prependLog("failed to spawn web page: " + e)
-        )
-      });
+      spawnWebPageContractWithParameters("html");
     }, false);
   }
 
@@ -72,7 +26,7 @@ window.onload = function () {
    *-------------------------------------------------------------------*/
 
   // STEP 1 : Select section of webpage
-  /*var checkPageSectionButton = document.getElementById('pagesection');
+  var checkPageSectionButton = document.getElementById('pagesection');
   if (checkPageSectionButton) {
     checkPageSectionButton.addEventListener('click', function () {
 
@@ -125,8 +79,6 @@ window.onload = function () {
     spawnWebPageContractWithParameters(pageSectionSelector);
 
   });
-}*/
-
 }
 
 /*---------------------------------------------------------------------
@@ -148,9 +100,6 @@ async function spawnWebPageContractWithParameters(selector: string) {
     // Retrieve TextOnly field
     const textOnlyBox = document.getElementById('txtOnly') as HTMLInputElement;
     var textOnly = textOnlyBox.checked
-
-    // ** SPAWN CONTRACT ** //
-    const p = document.getElementById('status');
 
     // Get public.toml
     await Handler.getInstance().LoadRoster(roster);
