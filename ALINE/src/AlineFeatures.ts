4c038a741c9e66a270c04faa65abcd35ba8acbb1
Unknown <charline.montial@epfl.ch>
2019-12-13 23:36:12 +0100

    Parse ID and content + factorize code

--- ALINE/src/AlineFeatures.ts
+++ ALINE/src/AlineFeatures.ts
@@ -1,20 +1,15 @@
-import { getDarc, loadSigner, addRule, spawnWebPage, hexStringToByte } from "./CothorityUtilities";
+import { spawnWebPage } from "./CothorityUtilities";
 import * as Cothority from "@dedis/cothority";
+import { displayErrorForUser, getInstanceAndCompare, RetrieveContentOfAttestation } from './AlineUtilities';
+import { getDarc, loadSigner} from "./CothorityUtilities";
 import { ContractWebPageData } from "./ContractWebPageData"
 import { Handler } from "./Handler";
 import { roster } from "./roster";
-import { WebPageInstance } from './WebPageInstance';
-import { displayErrorForUser } from './AlineUtilities';
 
 export {
   Cothority
 };
 
-//TODO: mettre le require en haut du fichier
-//@ts-ignore
-//import blake2 = require('blake2');
-var blake = require('blakejs')
-console.log(blake.blake2bHex('abc'))
 /*---------------------------------------------------------------------
  |  Spawn contract with correct parameters
  *-------------------------------------------------------------------*/
@@ -29,8 +24,21 @@ export async function spawnWebPageContractWithParameters(selector: string) {
     'lastFocusedWindow': true
   }, async function (tabs) {
 
+    // Init the skip chain
+    // Get public.toml
+    try {
+      await Handler.getInstance().LoadRoster(roster);
+    } catch (e) {
+
+    }
     var skipChainID = "8653fd3407a680dc41b569afcd329f61d0a0e528e66158d4cc3fc593d9d8e0fa";
     var privateKey = "ef31a12194cf70b960644d010e12a77a874bcc16ed20af3224a5ac40ad82ea03";
+
+    // Skip chain ID
+    await getDarc(skipChainID);
+
+    // Private Key
+    await loadSigner(privateKey);
 
     // Retrieve URL of current webpage
     var url = tabs[0].url;
@@ -38,22 +46,6 @@ export async function spawnWebPageContractWithParameters(selector: string) {
     // Retrieve TextOnly field
     const textOnlyBox = document.getElementById('txtOnly') as HTMLInputElement;
     var textOnly = textOnlyBox.checked
-
-    // Get public.toml
-    try {
-      await Handler.getInstance().LoadRoster(roster);
-    } catch (e) {
-
-    }
-    // Skip chain ID
-    await getDarc(skipChainID);
-
-    // Private Key
-    await loadSigner(privateKey);
-
-    // Add rule to spawn the webPage contract
-    // To do only once
-    //await addRule("spawn:webPage");
 
     // Spawn the webPage contract
     var contractWebPageData = new ContractWebPageData();
@@ -88,106 +80,32 @@ export async function uploadSubmitTextForms() {
   document.getElementById('positiveanswer').style.display = "none";
   document.getElementById('negativeanswer').style.display = "none";
 
-  // Retrieve text forms
-  var instanceIDStringElem = document.getElementById('uploadinstanceid') as HTMLInputElement;
-  var instanceIDString = instanceIDStringElem.value;
-  var contentElem = document.getElementById('uploadcontentid') as HTMLInputElement;
-  var content = contentElem.value;
-
-
-
-  //TODO: BLAKE2B PROBLEM
-  // TODO: FACTORIZE AND INITIALIZE WHEN POP UP OPENS
-  var skipChainID = "8653fd3407a680dc41b569afcd329f61d0a0e528e66158d4cc3fc593d9d8e0fa";
-  var privateKey = "ef31a12194cf70b960644d010e12a77a874bcc16ed20af3224a5ac40ad82ea03";
-
-  // Retrieve URL of current webpage
-
-  // Retrieve TextOnly field
-
-
+  // Init the skip chain
   // Get public.toml
   try {
     await Handler.getInstance().LoadRoster(roster);
   } catch (e) {
 
   }
+  var skipChainID = "8653fd3407a680dc41b569afcd329f61d0a0e528e66158d4cc3fc593d9d8e0fa";
+  var privateKey = "ef31a12194cf70b960644d010e12a77a874bcc16ed20af3224a5ac40ad82ea03";
+
   // Skip chain ID
   await getDarc(skipChainID);
 
   // Private Key
   await loadSigner(privateKey);
 
-
-  Handler.startLoader();
-  console.log("creating an RPC to get the key value instance...");
-  const rpc = Cothority.byzcoin.ByzCoinRPC.fromByzcoin(Handler.roster, Handler.scid);
-  rpc.then(
-    (r) => {
-      console.log("RPC created, we now send a get proof request...");
-      r.getProofFromLatest(Buffer.from(hexStringToByte(instanceIDString))).then(
-        (proof) => {
-          console.log("got the proof, let's check it...");
-          if (!proof.exists(Buffer.from(hexStringToByte(instanceIDString)))) {
-            console.log("this is not a proof of existence... aborting!");
-            displayErrorForUser();
-            return;
-          }
-          if (!proof.matchContract(WebPageInstance.contractID)) {
-            console.log("this is not a proof for the webpagecontrac... aborting!");
-            displayErrorForUser();
-            return;
-          }
-          console.log("ok, now let's decode it...");
-          const webpageInstance = ContractWebPageData.decode(proof.value);
-          console.log("here is the webpage instance: \n" + webpageInstance.toString());
-
-          /*---------------------------------------------------------------------
-         | Comparison of the two hashes
-         *-------------------------------------------------------------------*/
-          //webpageInstance.HashedContent.toString("hex")
-          var OUTPUT_LENGTH = 32 // bytes
-          var context = blake.blake2bInit(OUTPUT_LENGTH, null)
-          // each time you get a byte array from the stream:
-          var enc = new TextEncoder(); // always utf-8
-          console.log(enc.encode(content));
-          blake.blake2bUpdate(context, enc.encode(content));
-          // finally, once the stream has been exhausted
-          var hashToCheck = Buffer.from(blake.blake2bFinal(context)).toString("hex");
-          console.log("1st hash :");
-          console.log(hashToCheck);
-          console.log("2nd hash: ");
-          console.log(webpageInstance.HashedContent.toString("hex"));
-          if (webpageInstance.HashedContent.toString("hex").localeCompare(hashToCheck) == 0) {
-            document.getElementById('loadinggifid').style.visibility = "hidden";
-            document.getElementById('tickid').style.visibility = "visible";
-            document.getElementById('positiveanswer').innerText = "The content of this webpage has been correctly verified !";
-            document.getElementById('positiveanswer').style.display = "inline";
-
-          } else {
-            document.getElementById('loadinggifid').style.visibility = "hidden";
-            document.getElementById('crossid').style.visibility = "visible";
-            document.getElementById('negativeanswer').innerText = "The content of this webpage is not the same !";
-            document.getElementById('negativeanswer').style.display = "inline";
-          }
-        },
-        (e: Error) => {
-          console.error(e);
-          console.log("failed to get the key value instance: " + e);
-          displayErrorForUser();
-          throw "A problem occured.";
-        },
-      ).finally(
-        () => Handler.stopLoader(),
-      );
-    },
-    (e) => {
-      Handler.stopLoader();
-      console.log("failed to create RPC: " + e);
-      displayErrorForUser();
-      throw "A problem occured.";
-    },
-  );;
+  // Retrieve text forms
+  var textAreaContentElem = document.getElementById('uploadinstanceid') as HTMLInputElement;
+  var textAreaContentString = textAreaContentElem.value;
+  var instanceIDString = textAreaContentString.split("Instance ID:")[1].trim().split(" and Content:")[0].trim();
+  var content = textAreaContentString.split("Content:")[1].trim()
+  console.log("ID:");
+  console.log(instanceIDString);
+  console.log("Content:");
+  console.log(content);
+  getInstanceAndCompare(content, instanceIDString);
 }
 
 export async function downloadContentOfWebpage() {
@@ -199,53 +117,7 @@ export async function downloadContentOfWebpage() {
     var url = tabs[0].url;
     var domain = url.replace('www.', '').replace('http://', '').replace('https://', '').split(/[/?#]/)[0];
     var instanceIDString = document.getElementById('instanceid').innerText
-
-    Handler.startLoader();
-    console.log("creating an RPC to get the key value instance...");
-    const rpc = Cothority.byzcoin.ByzCoinRPC.fromByzcoin(Handler.roster, Handler.scid);
-    rpc.then(
-      (r) => {
-        console.log("RPC created, we now send a get proof request...");
-        r.getProofFromLatest(Buffer.from(hexStringToByte(instanceIDString))).then(
-          (proof) => {
-            console.log("got the proof, let's check it...");
-            if (!proof.exists(Buffer.from(hexStringToByte(instanceIDString)))) {
-              console.log("this is not a proof of existence... aborting!");
-              displayErrorForUser();
-              return;
-            }
-            if (!proof.matchContract(WebPageInstance.contractID)) {
-              console.log("this is not a proof for the webpagecontrac... aborting!");
-              displayErrorForUser();
-              return;
-            }
-            console.log("ok, now let's decode it...");
-            const webpageInstance = ContractWebPageData.decode(proof.value);
-            console.log("here is the webpage instance: \n" + webpageInstance.toString());
-            var blob = new Blob([webpageInstance.Content], { type: "text/plain" });
-            chrome.downloads.download({
-              url: URL.createObjectURL(blob),
-              filename: "Content of website " + domain
-            });
-            document.getElementById('loadinggifid').style.visibility = "hidden"
-          },
-          (e: Error) => {
-            console.error(e);
-            console.log("failed to get the key value instance: " + e);
-            displayErrorForUser();
-            throw "A problem occured.";
-          },
-        ).finally(
-          () => Handler.stopLoader(),
-        );
-      },
-      (e) => {
-        Handler.stopLoader();
-        console.log("failed to create RPC: " + e);
-        displayErrorForUser();
-        throw "A problem occured.";
-      },
-    );
+    RetrieveContentOfAttestation(domain, instanceIDString);
   });
 }
 
@@ -266,3 +138,4 @@ export async function downloadInfosOfAttestation() {
     });
   });
 }
+
